1d47689d3ba065fc6a9d24f70a0748b1560ff675
Ruan Santos Sacramento <ruansantossacramento32@gmail.com>
2026-04-25 14:20:44 -0300

    feat: adiciona pacote de introdução à Orientação a Objetos
    Cria nova estrutura de pacotes para aulas iniciais de POO,
    organizando o conteúdo separado do módulo de introdução anterior.

---
+++ src/cursomaratonajava/introducao/Aula01Comentários.java
@@ -0,0 +1,18 @@
+package cursomaratonajava.introducao;
+
+/**
+ * Esse é um comentário
+ * javadoc
+ */
+public class Aula01Comentários {
+    //Estou fazendo meu primeiro comentario
+    /*
+    Esse é um comentário
+     de
+     multiplas linhas
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+ 
