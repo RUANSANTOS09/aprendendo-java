5d802f88b4ed5171747a865d958edd12ba86e5a3
Ruan Santos Sacramento <ruansantossacramento32@gmail.com>
2026-04-30 14:58:12 -0300

    feat: Reorganizando pasta javacore, novas aulas

--- src/cursomaratonajava/javacore/sobrecargametodos/test/AnimeTest01.java
+++ src/cursomaratonajava/javacore/sobrecargametodos/test/AnimeTest01.java
@@ -5,7 +5,7 @@ import cursomaratonajava.javacore.sobrecargametodos.dominio.Anime;
 public class AnimeTest01 {
     public static void main(String[] args) {
         Anime anime = new Anime();
-        anime.init("Demon Slayer", "Demográfia", 120, "Ação");
+        anime.init("Dragon Ball", "Demográfia", 120, "Ação");
         anime.imprime();
     }
 }
